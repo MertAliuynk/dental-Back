887766c9813fdf8df976abeb6b3d6da607e8c3b7
MertAliuynk <mertuynk3@gmail.com>
2025-08-08 22:55:12 +0300

    inputhelpers.js dosyasında bcryptjs yerine bcrypt kullanıldı

--- helpers/db/scripts/runMigrations.js
+++ helpers/db/scripts/runMigrations.js
@@ -6,11 +6,29 @@ async function runMigrations() {
   try {
     console.log('Starting migrations...');
 
-    // Dinamik olarak migrations klasöründeki tüm .sql dosyalarını sırala
+    // Elle belirlenmiş migration sırası (bağımlılıklara göre)
     const migrationDir = path.join(__dirname, '../migrations');
-    const allFiles = fs.readdirSync(migrationDir)
-      .filter((f) => f.endsWith('.sql'))
-      .sort(); // isimlere göre kronolojik sıralama varsayılır
+    const allFiles = [
+      '20250728_1_create_branches_table.sql',
+      '20250728_2_create_users_table.sql',
+      '20250728_3_create_patients_table.sql',
+      '20250728_4_create_patient_anamnesis_table.sql',
+      '20250728_5_create_treatment_types_table.sql',
+      '20250728_6_create_price_lists_table.sql',
+      '20250728_7_create_price_list_items_table.sql',
+      '20250728_8_create_treatments_table.sql',
+      '20250728_9_create_appointments_table.sql',
+      '20250728_10_create_sms_templates_table.sql',
+      '20250728_11_create_sms_schedules_table.sql',
+      '20250728_12_create_feedbacks_table.sql',
+      '20250728_13_create_consent_forms_table.sql',
+      '20250728_14_add_notes_to_treatments.sql',
+      '20250728_15_update_treatment_status_turkish.sql',
+      '20250807_1_update_appointments_status_constraint.sql',
+      '20250807_2_update_tc_number_length.sql',
+      '20250807_3_create_feedback_planning.sql',
+      '20250808_1_fix_feedback_trigger.sql',
+    ];
 
     // Migrations tablosunu önce oluştur (eğer yoksa)
     await pool.query(`
